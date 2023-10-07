71e170c95a60b64c9a20be5699eaa0941e4e08fe
YongBin123 <134280533+YongBin123@users.noreply.github.com>
2023-10-07 15:03:56 +0900

    [Feat] array, object state 변경 추가
    리액트 강의 중 array, object state 변경 부분 실습 결과입니다!

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, 글제목변경] = useState(['남자코트 추천', '여자코트 추천', '아동코트 추천'])
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬 독학'])
   let [따봉, 따봉변경] = useState(0);
 
   return (
@@ -15,8 +15,16 @@ function App() {
       </div>
 
       <button onClick={()=>{
-        글제목변경(['여자코트 추천', '남자코트 추천', '아동코트 추천']);
-      }}>제목변경</button>
+        let copy = [...글제목];
+        copy[0] = '여자코트 추천'
+        글제목변경(copy);
+      }}>글수정</button>
+
+      <button onClick={()=>{
+        let copy = [...글제목];
+        copy.sort();
+        글제목변경(copy);
+      }}>정렬버튼</button>
       
       <div className="list">
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
