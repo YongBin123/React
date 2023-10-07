414fae53dfac67c6fc314f0106e1c9ab9fc49863
YongBin123 <134280533+YongBin123@users.noreply.github.com>
2023-10-07 13:39:34 +0900

    [Feat] 버튼 기능 및 state 변경 추가
    리액트 영상 중 버튼 기능 및 state 변경 추가 부분 실습과 숙제 코드 추가했습니다!

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집';
-  let [글제목, b] = useState(['남자코트 추천', '여자코트 추천', '아동코트추천'])
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '여자코트 추천', '아동코트 추천'])
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div class="black-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button onClick={()=>{
+        글제목변경(['여자코트 추천', '남자코트 추천', '아동코트 추천']);
+      }}>제목변경</button>
+      
       <div className="list">
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
